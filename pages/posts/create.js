b5c29eb4b4b9dd2730af140810c1e986bed79740
sincanmaulanaa <sincanmaulanaa@gmail.com>
2022-08-16 22:12:41 +0700

    💄 Add Nav component

--- pages/posts/create.js
+++ pages/posts/create.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { authorizationPage } from "../../middlewares/authorizationPage";
 import Router from "next/router";
+import Nav from "../../components/Nav";
 
 export async function getServerSideProps(context) {
   const { token } = await authorizationPage(context);
@@ -54,65 +55,67 @@ export default function PostCreate(props) {
     });
   }
   return (
-    <div className="max-w-6xl mx-auto p-10 md:p-20">
-      <form
-        className="space-y-8 divide-y divide-gray-200"
-        onSubmit={createHandler.bind(this)}
-      >
-        <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
-          <div>
+    <>
+      <Nav />
+      <div className="max-w-6xl mx-auto p-10 md:p-20">
+        <form
+          className="space-y-8 divide-y divide-gray-200"
+          onSubmit={createHandler.bind(this)}
+        >
+          <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
             <div>
-              <h3 className="text-lg leading-6 font-medium text-gray-900">
-                Buat Postingan
-              </h3>
-              <p className="mt-1 max-w-2xl text-sm text-gray-500">
-                Postingan ini akan ditampilkan secara publik setelah kamu
-                terbitkan.
-              </p>
-            </div>
-
-            <div className="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
-              <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                <label
-                  htmlFor="title"
-                  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-                >
-                  {" "}
-                  Judul Postingan{" "}
-                </label>
-                <div className="mt-1 sm:mt-0 sm:col-span-2">
-                  <input
-                    type="text"
-                    name="title"
-                    id="title"
-                    className="max-w-lg block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
-                    onChange={fieldHandler.bind(this)}
-                  />
-                </div>
+              <div>
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                  Buat Postingan
+                </h3>
+                <p className="mt-1 max-w-2xl text-sm text-gray-500">
+                  Postingan ini akan ditampilkan secara publik setelah kamu
+                  terbitkan.
+                </p>
               </div>
 
-              <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-                <label
-                  htmlFor="content"
-                  className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-                >
-                  {" "}
-                  Isi Postingan{" "}
-                </label>
-                <div className="mt-1 sm:mt-0 sm:col-span-2">
-                  <textarea
-                    id="content"
-                    name="content"
-                    rows="3"
-                    className="max-w-lg shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border border-gray-300 rounded-md"
-                    onChange={fieldHandler.bind(this)}
-                  ></textarea>
-                  <p className="mt-2 text-sm text-gray-500">
-                    Tulis beberapa kalimat untuk postingan ini.
-                  </p>
+              <div className="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
+                <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                  <label
+                    htmlFor="title"
+                    className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                  >
+                    {" "}
+                    Judul Postingan{" "}
+                  </label>
+                  <div className="mt-1 sm:mt-0 sm:col-span-2">
+                    <input
+                      type="text"
+                      name="title"
+                      id="title"
+                      className="max-w-lg block w-full shadow-sm focus:ring-blue-500 focus:border-blue-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md"
+                      onChange={fieldHandler.bind(this)}
+                    />
+                  </div>
                 </div>
-              </div>
-              {/* <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+
+                <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+                  <label
+                    htmlFor="content"
+                    className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+                  >
+                    {" "}
+                    Isi Postingan{" "}
+                  </label>
+                  <div className="mt-1 sm:mt-0 sm:col-span-2">
+                    <textarea
+                      id="content"
+                      name="content"
+                      rows="3"
+                      className="max-w-lg shadow-sm block w-full focus:ring-blue-500 focus:border-blue-500 sm:text-sm border border-gray-300 rounded-md"
+                      onChange={fieldHandler.bind(this)}
+                    ></textarea>
+                    <p className="mt-2 text-sm text-gray-500">
+                      Tulis beberapa kalimat untuk postingan ini.
+                    </p>
+                  </div>
+                </div>
+                {/* <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
               <label
                 htmlFor="cover-photo"
                 className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
@@ -159,22 +162,23 @@ export default function PostCreate(props) {
                 </div>
               </div>
             </div> */}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="pt-5">
-          <div className="flex justify-end">
-            <button
-              type="submit"
-              className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              Terbitkan
-            </button>
+          <div className="pt-5">
+            <div className="flex justify-end">
+              <button
+                type="submit"
+                className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              >
+                Terbitkan
+              </button>
+            </div>
           </div>
-        </div>
-        <div>status: {status}</div>
-      </form>
-    </div>
+          <div>status: {status}</div>
+        </form>
+      </div>
+    </>
   );
 }
